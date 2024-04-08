f9270d36bd4245d49e86d615c8a522572aff9498
Teakz9 <ybreton27@gmail.com>
2024-04-08 16:15:25 +0200

    Footer complet et responsive

--- src/app/propos/propos.component.ts
+++ src/app/propos/propos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-propos',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './propos.component.html',
   styleUrl: './propos.component.css'
 })
