34e41ed20bc97064df064001b3a896dfab7ce52f
Teakz9 <ybreton27@gmail.com>
2024-04-16 17:40:53 +0200

    Page des produits détaillés responsive

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -6,16 +6,16 @@ import { Injectable } from '@angular/core';
 export class ProductsService {
 
   products = [
-    {id: 1, imageUrl: 'assets/images/Figurine-Asterix.jpg', name:'Astérix', price: 35.00},
-    {id: 2, imageUrl: 'assets/images/Figurine-Obelix.jpg', name:'Obélix', price: 40.00},
-    {id: 3, imageUrl: 'assets/images/Figurine-Idefix.jpg', name:'Idéfix', price: 32.00},
-    {id: 4, imageUrl: 'assets/images/Figurine-Panoramix.jpg', name:'Panoramix', price: 30.00},
-    {id: 5, imageUrl: 'assets/images/Figurine-Abraracourcix.jpg', name:'Abraracourcix', price: 33.00},
-    {id: 6, imageUrl: 'assets/images/Figurine-Bonemine.jpg', name:'Bonemine', price: 28.00},
-    {id: 7, imageUrl: 'assets/images/Figurine-Cetautomatix.jpg', name:'Cetautomatix', price: 26.00},
-    {id: 8, imageUrl: 'assets/images/Figurine-Falbala.jpg', name:'Falbala', price: 35.00},
-    {id: 9, imageUrl: 'assets/images/Figurine-JulesCesar.jpg', name:'Jules César', price: 34.00},
-    {id: 10, imageUrl: 'assets/images/Figurine-Agecanomix.jpg', name:'Agecanomix', price: 24.00},
+    {id: 1, imageUrl: 'assets/images/Figurine-Asterix.jpg', name:'Astérix', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 35.00},
+    {id: 2, imageUrl: 'assets/images/Figurine-Obelix.jpg', name:'Obélix', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 40.00},
+    {id: 3, imageUrl: 'assets/images/Figurine-Idefix.jpg', name:'Idéfix', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 32.00},
+    {id: 4, imageUrl: 'assets/images/Figurine-Panoramix.jpg', name:'Panoramix', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 30.00},
+    {id: 5, imageUrl: 'assets/images/Figurine-Abraracourcix.jpg', name:'Abraracourcix', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 33.00},
+    {id: 6, imageUrl: 'assets/images/Figurine-Bonemine.jpg', name:'Bonemine', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 28.00},
+    {id: 7, imageUrl: 'assets/images/Figurine-Cetautomatix.jpg', name:'Cetautomatix', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 26.00},
+    {id: 8, imageUrl: 'assets/images/Figurine-Falbala.jpg', name:'Falbala', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 35.00},
+    {id: 9, imageUrl: 'assets/images/Figurine-JulesCesar.jpg', name:'Jules César', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 34.00},
+    {id: 10, imageUrl: 'assets/images/Figurine-Agecanomix.jpg', name:'Agecanomix', description:'Nos figurines sont faites à la main, avec des matériaux recyclables et de la peinture non toxique pour notre environnement. Nos produits sont certifiés 100% Français.', price: 24.00},
   ]
 
   constructor() { }
